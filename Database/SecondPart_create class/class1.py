d37f8178a1b9f2169b359f18948e801eb6c4a6d1
Hubert51 <gengr@rpi.edu>
2016-11-10 16:47:47 -0500

    Simplify
    Simplify the process of create a table. Use variable “commend” to
    finish the sentence about query. Then use execute to create table
    directly

--- Database/SecondPart_create class/class1.py
+++ Database/SecondPart_create class/class1.py
@@ -11,9 +11,6 @@ import sqlite3
 
 
 class DatabaseIO:
-    l12 = [1, 2, 3]
-    d_table = {}
-
     def __init__(self, name):
         """
         In my opinion, this is an initial part. If I have to use variable from
@@ -25,76 +22,33 @@ class DatabaseIO:
         self.con = sqlite3.connect(name)
         self.cur = self.con.cursor()
         d_table = {}
+        
 
     def create_table1(self):
         table_name = raw_input("Please entre the table name ==> ")
-        i = 0
         l = []  # This list stores the info which is used to create table
-        l2 = []  # This list stores the table info which will be useful when we need to know the columns of  table
-        d_table = dict()
-        check = True
-        while check:
+        while 1 > 0:
             column = raw_input('''Please entre column with this data tpye such as 
 INTEGER or TXT (if finish, type: end) ==> ''')
             if column != "end":
                 l.append(column)
-                l2.append(column)
             else:
                 break
-
-        d_table[table_name] = l2
         key = raw_input("Please enter the key ==> ")
-        num = len(l)
-
-        if num == 1:
-            self.cur.execute("""CREATE TABLE '%s' (
-                           {:s} NOT NULL,
-                           PRIMARY KEY ({:s}))""".foramt(table_name, l[0], key))
-            self.con.commit()
-
-        elif num == 2 :
-            self.cur.execute("""CREATE TABLE {:s} (
-                           {:s} NOT NULL,
-                           {:s} NOT NULL,
-                           PRIMARY KEY ({:s})),""".format(table_name, l[0], l[1], key))
-            self.con.commit()
-
-        elif num == 3 and table_name == "course":
-            self.cur.execute("""CREATE TABLE {:s} (
-                           {:s} NOT NULL,
-                           {:s} NOT NULL,
-                           {:s} NOT NULL,
-                           PRIMARY KEY ({:}),
-                           FOREIGN KEY(rin) REFERENCES student(rin))""".format(table_name, l[0], l[1], l[2], key))
-            self.con.commit()
+        num = len(l)        
+        command = "CREATE TABLE {:} (".format(table_name)
         
-        elif num == 3 :
-            self.cur.execute("""CREATE TABLE {:s} (
-                           {:s} NOT NULL,
-                           {:s} NOT NULL,
-                           {:s} NOT NULL,
-                           PRIMARY KEY ({:s}))""".format(table_name, l[0], l[1], l[2], key))
-            self.con.commit()            
         
-
-        elif num == 4:
-            self.cur.execute("""CREATE TABLE {:s} (
-                           {:s} NOT NULL,
-                           {:s} NOT NULL,
-                           {:s} NOT NULL,
-                           {:s} NOT NULL,
-                           PRIMARY KEY ({:s}))""".format(table_name, l[0], l[1], l[2], l[3], key))
-            self.con.commit()
-        elif num == 5:
-            self.cur.execute("""CREATE TABLE {:s} (
-                           {:s} NOT NULL,
-                           {:s} NOT NULL,
-                           {:s} NOT NULL,
-                           {:s} NOT NULL,
-                           {:s} NOT NULL,
-                           PRIMARY KEY ({:s}))""".format(table_name, l[0], l[1], l[2], l[3], l[4], key))
-            self.con.commit()
-
+        
+        for i in l:
+            command += "{:} NOT NULL,".format(i)
+            
+        command += "PRIMARY KEY ({:}))".format(key)
+               
+        self.cur.execute(command)
+        self.con.commit()        
+    
+    
     def add_data(self):
         table_name = raw_input("Please enter the table name which you want to add data ==> ")
         
